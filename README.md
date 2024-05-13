d252a652f970a80864d0cba16c0a94366db70c00
Marco Luzzara <marco.luzzara@hotmail.it>
2024-05-13 14:16:39 +0200

    add docker-compose with kafka brokers and app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # prin
+
+## Sources
+
+- [How to (actually) Configure a Kafka Cluster in KRaft Mode For Testing & Development](https://medium.com/@hjdjoo/how-to-actually-configure-a-kafka-cluster-in-kraft-mode-for-testing-development-8f90f09e36b1)
+- [The Apache Kafka Control Plane](https://developer.confluent.io/courses/architecture/control-plane/)
--- remote_machine/main.py
+++ remote_machine/main.py
@@ -1,14 +1,29 @@
 import sys
 import time
+import os
 import logging
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler, FileSystemEvent
 
-import pandas as pd 
+import pandas as pd
 
-class FileProcessor:
-    def __init__(self, path: str):
-        self.path = path
+from confluent_kafka import Producer
+import socket
+
+def get_env_or_throw_if_empty(env: str) -> str:
+    env_value = os.getenv(env)
+    if env_value is not None and len(env_value) > 0:
+        return env_value
+    else:
+        raise f'environment variable {env} must be non-empty'
+
+WATCHED_FOLDER = get_env_or_throw_if_empty('WATCHED_FOLDER')
+PATIENT_ID = get_env_or_throw_if_empty('PATIENT_ID')
+KAFKA_BOOTSTRAP_SERVERS = get_env_or_throw_if_empty('KAFKA_BOOTSTRAP_SERVERS')
+
+class EventProcessor:
+    def __init__(self, event: FileSystemEvent):
+        self.path = event.src_path
 
     def process(self) -> bytes:
         df = pd.read_excel(self.path)
@@ -16,33 +31,55 @@ class FileProcessor:
         csv_file_bytes = csv_file.encode('utf-8')
 
         return csv_file_bytes
+    
+class FileProcessor:
+    def process(self, file: bytes) -> None:
+        raise 'subclass me'
+
+class KafkaFileProcessor(FileProcessor):
+    def __init__(self, producer: Producer):
+        self.producer = producer
+
+    def process(self, file: bytes) -> None:
+        self.producer.produce('filesystemwatcher.medical-records', key=PATIENT_ID, value=file)
+
 
 class XslFileEventHandler(PatternMatchingEventHandler):
-    def __init__(self):
+    def __init__(self, fileProcessor: FileProcessor):
         #include only the excel files, the events for other file types are ignored
         super().__init__(['*.xls', '*.xlsx', '*.ods'], ignore_directories=True)
+        self.fileProcessor = fileProcessor
 
     def on_created(self, event: FileSystemEvent) -> None:
-        fileProcessor = FileProcessor(event.src_path)
-        csv_file_bytes = fileProcessor.process()
+        eventProcessor = EventProcessor(event)
+        csv_file_bytes = eventProcessor.process()
 
         # show only the first 10 byte of a CSV
         csv_overview = str(csv_file_bytes[0:10]) + ('...' if len(csv_file_bytes) > 10 else '')
         logging.info(f'Excel file found: {event.src_path}: {csv_overview}')
 
-        # TODO: publish on kafka
+        self.fileProcessor.process(csv_file_bytes)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    logging.info(f'Start watching directory {path!r}')
 
-    event_handler = XslFileEventHandler()
+    # bootstrap.servers does not require the complete list of nodes
+    # https://stackoverflow.com/q/61656223/5587393
+    kafka_conf = {
+        'bootstrap.servers': KAFKA_BOOTSTRAP_SERVERS,
+        'client.id': socket.gethostname()
+    }
+    kafka_producer = Producer(kafka_conf)
+
+    logging.info(f'Start watching directory {WATCHED_FOLDER!r}')
+
+    fileProcessor = KafkaFileProcessor(kafka_producer)
+    event_handler = XslFileEventHandler(fileProcessor)
     
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=False)
+    observer.schedule(event_handler, WATCHED_FOLDER, recursive=False)
     observer.start()
     try:
         while True:
