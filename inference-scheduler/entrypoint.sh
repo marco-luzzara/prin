bb9ca5dad421cfffe5f40cf6f65760796e514cf1
Marco Luzzara <marco.luzzara@hotmail.it>
2025-05-14 10:24:44 +0200

    model inference container is now attached to the compose network

--- inference-scheduler/entrypoint.sh
+++ inference-scheduler/entrypoint.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
 function start_container {
-    CONTAINER_ID="$(curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" \
-        -d "{\"Image\": \"$INFERENCE_DOCKER_IMAGE\", \"Cmd\": [\"python\", \"main.py\"]}" \
-        -X POST http://localhost/containers/create | jq -r '.Id')"
+    # https://stackoverflow.com/a/78009946/5587393
+    THIS_CONTAINER_ID="$(cat /proc/self/mountinfo | grep -m1 -oE 'docker/containers/([a-f0-9]+)/' |\
+        xargs basename)"
 
+    # the spawned container must be attached to the same network as this container
+    # because it needs to reach trino (or any other service in the same Docker compose)
+    THIS_CONTAINER_NETWORK="$(curl -s --unix-socket /var/run/docker.sock -X GET \
+        "http://localhost/containers/$THIS_CONTAINER_ID/json" | \
+        jq -r '.NetworkSettings.Networks | keys | .[0]')"
+
+    # create new container
+    SPAWNED_CONTAINER_ID="$(
+    {
+        curl -s --unix-socket /var/run/docker.sock \
+        -H "Content-Type: application/json" \
+        -d @- \
+        -X POST http://localhost/containers/create <<JSON
+{
+    "Image": "$INFERENCE_DOCKER_IMAGE",
+    "Cmd": ["python", "main.py"],
+    "NetworkingConfig": {
+        "EndpointsConfig": {
+            "$THIS_CONTAINER_NETWORK": {}
+        }
+    }
+}
+JSON
+    } | jq -r '.Id'
+    )"
     echo "$(date -u +"%Y-%m-%dT%H:%M:%S") - Inference container created"
 
-    curl -s --unix-socket /var/run/docker.sock -X POST "http://localhost/containers/$CONTAINER_ID/start"
-
+    # start container
+    curl -s --unix-socket /var/run/docker.sock -X POST \
+        "http://localhost/containers/$SPAWNED_CONTAINER_ID/start"
     echo "$(date -u +"%Y-%m-%dT%H:%M:%S") - Inference container started"
 }
 
