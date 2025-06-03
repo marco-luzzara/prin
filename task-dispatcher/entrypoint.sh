79ee69a3b16d52eda07e2bedf939f21c2e92b718
Marco Luzzara <marco.luzzara@hotmail.it>
2025-06-03 12:23:29 +0200

    task-dispatcher gets the task entrypoint from the kafka message

--- task-dispatcher/entrypoint.sh
+++ task-dispatcher/entrypoint.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
+# https://stackoverflow.com/a/78009946/5587393
+THIS_CONTAINER_ID="$(cat /proc/self/mountinfo | grep -m1 -oE 'docker/containers/([a-f0-9]+)/' |\
+    xargs basename)"
+
+# the spawned containers must be attached to the same network as this container
+# because it needs to reach trino (or any other service in the same Docker compose network)
+THIS_CONTAINER_NETWORK="$(curl -s --unix-socket /var/run/docker.sock -X GET \
+    "http://localhost/containers/$THIS_CONTAINER_ID/json" | \
+    jq -r '.NetworkSettings.Networks | keys | .[0]')"
+
 function log_with_date {
     local msg="$1"
     echo "$(date -u +"%Y-%m-%dT%H:%M:%S") - $msg"
 }
 
 function start_container {
-    local trinoUser="$1"
-    log_with_date "Inference started by user $trinoUser"
+    local msg="$1"
+    local trinoUser="$(echo "$msg" | jq -r '.user')"
+    local trinoGroup="$(echo "$msg" | jq -r '.group')"
+    local taskEntrypoint="$(echo "$msg" | jq -r '.params.entrypoint')"
 
-    # https://stackoverflow.com/a/78009946/5587393
-    THIS_CONTAINER_ID="$(cat /proc/self/mountinfo | grep -m1 -oE 'docker/containers/([a-f0-9]+)/' |\
-        xargs basename)"
-
-    # the spawned container must be attached to the same network as this container
-    # because it needs to reach trino (or any other service in the same Docker compose)
-    THIS_CONTAINER_NETWORK="$(curl -s --unix-socket /var/run/docker.sock -X GET \
-        "http://localhost/containers/$THIS_CONTAINER_ID/json" | \
-        jq -r '.NetworkSettings.Networks | keys | .[0]')"
+    log_with_date "Task started by user:group $trinoUser:$trinoGroup"
 
     # create new container
-    SPAWNED_CONTAINER_ID="$(
+    local spawnedContainerId="$(
     {
         curl -s --unix-socket /var/run/docker.sock \
         -H "Content-Type: application/json" \
         -d @- \
         -X POST http://localhost/containers/create <<JSON
 {
-    "Image": "$INFERENCE_DOCKER_IMAGE",
-    "Cmd": ["python", "main.py"],
+    "Image": "$TASK_DOCKER_IMAGE",
+    "Cmd": $taskEntrypoint,
     "Env": [
-        "TRINO_USER=$trinoUser"
+        "TRINO_USER=$trinoUser",
+        "GROUP=$trinoGroup"
     ],
     "NetworkingConfig": {
         "EndpointsConfig": {
@@ -41,12 +46,12 @@ function start_container {
 JSON
     } | jq -r '.Id'
     )"
-    log_with_date "Inference container created"
+    log_with_date "Task container created"
 
     # start container
     curl -s --unix-socket /var/run/docker.sock -X POST \
-        "http://localhost/containers/$SPAWNED_CONTAINER_ID/start"
-    log_with_date "Inference container started, remove it with \`docker container rm $SPAWNED_CONTAINER_ID\`"
+        "http://localhost/containers/$spawnedContainerId/start"
+    log_with_date "Task container started, remove it with \`docker container rm $spawnedContainerId\`"
 }
 
 /opt/kafka/bin/kafka-console-consumer.sh \
@@ -55,6 +60,5 @@ JSON
 while read -r msg
 do
     log_with_date "Message received: $msg"
-    trinoUser="$(echo "$msg" | jq -r '.username')"
-    start_container "$trinoUser"
+    start_container "$msg"
 done
